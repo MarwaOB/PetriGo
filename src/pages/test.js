b81f36b195e70f464ce5cffeae8dd35a93f1da6e
MarwaOB <mm_ogab@esi.dz>
2025-06-17 00:05:46 +0100

    Fixed really

--- src/pages/test.js
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public double[] convertTemperature(double celsius) {
-        while ( celsius < 0 || celsius >1000){
-            System.out.print("celsius = ");
-            scanf("%f", &celsius);
-            }
-        return ; 
-    }
-}
